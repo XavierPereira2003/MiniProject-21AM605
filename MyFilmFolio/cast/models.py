b420bc5fb611d1e916cccfa088753e35d16547e8
Deon Xavier Pereira <deonxavier2019@gmail.com>
2024-03-30 12:35:53 +0530

    update to database

--- MyFilmFolio/cast/models.py
+++ MyFilmFolio/cast/models.py
@@ -1,3 +1,13 @@
-from django.db import models
 
+from django.db import models
+from django.db.models import Model
 # Create your models here.
+
+class cast(Model):
+    cast_id=models.BigAutoField(primary_key=True)
+    name=models.CharField(max_length=255)
+    DoB=models.DateField()
+    Image=models.ImageField()
+    
+    def __str__(self):
+        return self.name
