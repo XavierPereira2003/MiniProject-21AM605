f3017eb4e0431f131c94e3a8d27021724fd86e2b
Deon Xavier Pereira <deonxavier2019@gmail.com>
2024-03-30 12:34:18 +0530

    Initiated the models movies and cast

--- MyFilmFolio/MyFilmFolio/settings.py
+++ MyFilmFolio/MyFilmFolio/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main',
+    'movies',
+    'cast'
 ]
 
 MIDDLEWARE = [
@@ -111,7 +113,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'en-uk'
 
 TIME_ZONE = 'UTC'
 
--- MyFilmFolio/movies/models.py
+++ MyFilmFolio/movies/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from django.db import models, Model
 
+
+MOVIE_GENRES = [
+    ("action", "Action"),
+    ("adventure", "Adventure"),
+    ("comedy", "Comedy"),
+    ("drama", "Drama"),
+    ("horror", "Horror"),
+    ("science_fiction", "Science Fiction"),
+    ("fantasy", "Fantasy"),
+    ("romance", "Romance"),
+    ("thriller", "Thriller"),
+    ("documentary", "Documentary"),
+    ("crime", "Crime"),
+    ("mystery", "Mystery"),
+    ("animation", "Animation"),
+    ("family", "Family"),
+    ("biography", "Biography"),
+    ("history", "History"),
+    ("war", "War"),
+    ("musical", "Musical"),
+    ("western", "Western"),
+    ("film_noir", "Film Noir"),
+]
 # Create your models here.
+
+class Movies(Model):
+    """
+    A class representing a movie in the database.
+    """
+    movie_id=models.BigAutoField(primary_key=True)
+    title=models.CharField(max_length=255)
+    genre=models.CharField(max_length=255, choices=MOVIE_GENRES)   
+    discription=models.TextField()
+    release_date=models.DateField()
+    poster_path=models.ImageField()
+
+    def __str__(self):
+        return "_".join(self.title.split())+str(self.movie_id)
+    
+    
