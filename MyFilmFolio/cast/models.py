f062d3b0274c7d32d0d916d5703521c1304cdef8
Deon Xavier Pereira <deonxavier2019@gmail.com>
2024-04-01 14:21:54 +0530

    Migrated database

--- MyFilmFolio/cast/models.py
+++ MyFilmFolio/cast/models.py
@@ -1,13 +1,17 @@
 
 from django.db import models
 from django.db.models import Model
+from django.utils.text import slugify
 # Create your models here.
 
-class cast(Model):
+class Cast(Model):
+    """
+    Represents a cast member in the database.   
+    """
     cast_id=models.BigAutoField(primary_key=True)
     name=models.CharField(max_length=255)
     DoB=models.DateField()
     Image=models.ImageField()
-    
+
     def __str__(self):
-        return self.name
+        return f"{slugify(self.name)}-{str(self.cast_id)}"
