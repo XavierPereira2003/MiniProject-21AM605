f4808d732132e198ab80437f385808be0af207c7
Deon Xavier Pereira <deonxavier2019@gmail.com>
2024-04-01 22:49:33 +0530

    linked urls-not working

--- MyFilmFolio/cast/urls.py
+++ MyFilmFolio/cast/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name='cast'
 urlpatterns = [
-    path('cast/',views.actor_list, name='cast'),
-    path("cast/<>")
+    path('cast/',views.actor_list, name='cast_home'),
+    path("cast/<slug>:slug", views.view_actor, name="cast_detail"),
 ]
