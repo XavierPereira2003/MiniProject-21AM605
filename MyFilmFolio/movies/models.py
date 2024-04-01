a2adf680241620470d5eaf0aaf5b6f595914242d
Deon Xavier Pereira <deonxavier2019@gmail.com>
2024-04-01 14:21:54 +0530

    Migrated database

--- MyFilmFolio/movies/models.py
+++ MyFilmFolio/movies/models.py
@@ -1,6 +1,7 @@
 from django.db import models
-from django.db import models, Model
-
+from django.db.models import Model
+from django.utils.text import slugify
+from cast.models import Cast
 
 MOVIE_GENRES = [
     ("action", "Action"),
@@ -33,11 +34,20 @@ class Movies(Model):
     movie_id=models.BigAutoField(primary_key=True)
     title=models.CharField(max_length=255)
     genre=models.CharField(max_length=255, choices=MOVIE_GENRES)   
+    director=models.ForeignKey(Cast, on_delete=models.DO_NOTHING)
     discription=models.TextField()
     release_date=models.DateField()
     poster_path=models.ImageField()
 
     def __str__(self):
-        return "_".join(self.title.split())+str(self.movie_id)
+        return f"{slugify(self.title)}-{str(self.movie_id)}"
     
     
+class Roles(Model):
+    """
+    A class representing a role played by a cast member in a movie.
+    """
+    role_id=models.BigAutoField(primary_key=True)
+    movie=models.ForeignKey(Movies, on_delete=models.DO_NOTHING)
+    cast=models.ForeignKey(Cast, on_delete=models.DO_NOTHING)
+    role_name=models.CharField(max_length=255)
