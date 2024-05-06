e1f179b7f2413d0c62fc2e58472bfa33c9f5242f
Anant <90376578+Antverse1@users.noreply.github.com>
2024-05-06 16:23:24 +0530

    basic site nav setup

--- MyFilmFolio/cast/urls.py
+++ MyFilmFolio/cast/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
+from movies.views import movie_detail
+
 
 app_name='cast'
 urlpatterns = [
-    path('',views.actors, name='cast_home'),
-    # path("<slug>:slug", views.view_actor, name="cast_detail"),
+    path('<int:cast_id>/', views.cast_view, name='cast-view'),
+    path('movies/<int:movie_id>/', movie_detail, name='movie_detail'),
 ]
--- MyFilmFolio/movies/urls.py
+++ MyFilmFolio/movies/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import movie_detail
+from cast.views import cast_view
 
 urlpatterns = [
     path('<int:movie_id>/', movie_detail, name='movie_detail'),
+    path('cast/<int:cast_id>/', cast_view, name='cast-view')
 ]
