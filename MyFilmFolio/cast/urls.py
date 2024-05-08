6e0e0b4b600de9e376cf0761e080b4a394121cdb
Anant <90376578+Antverse1@users.noreply.github.com>
2024-05-08 15:52:02 +0530

    Small Changes

--- MyFilmFolio/cast/urls.py
+++ MyFilmFolio/cast/urls.py
@@ -4,6 +4,7 @@ from movies.views import movie_detail
 
 
 app_name='cast'
+
 urlpatterns = [
     path('<int:cast_id>/', views.cast_view, name='cast-view'),
     path('movies/<int:movie_id>/', movie_detail, name='movie_detail'),
